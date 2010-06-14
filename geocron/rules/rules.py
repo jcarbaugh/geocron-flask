07f85bea1b3857692779b0b2b85a71cfac0edf99
kaitlin <kaitlin@kaitlin-desktop.(none)>
2010-06-14 17:30:27 -0400

    added rule check function

--- geocron/rules/rules.py
+++ geocron/rules/rules.py
@@ -15,8 +15,8 @@ db = connection[DATABASE_NAME]
 #   'email_address': 'receiver@mail.net',
 #   'email_text': 'come pick me up!',
 #   'valid_days': [1, 2, 3, 4, 5],
-#   'valid_start_time': 06:00,
-#   'valid_end_time': 13:00,
+#   'valid_start_time': '06:00',
+#   'valid_end_time': '13:00',
 # }
 #
 # expected format: (webhook)
@@ -26,10 +26,18 @@ db = connection[DATABASE_NAME]
 #   'method': 'POST',
 #   'callback_url': 'http://kaitlin.com/callback?action=iamhome',
 #   'valid_days': [1, 2, 3, 4, 5],
-#   'valid_start_time': 06:00,
-#   'valid_end_time': 13:00,
+#   'valid_start_time': '06:00',
+#   'valid_end_time': '13:00',
 # }
-
+def convert_from_unicode(data):
+    if isinstance(data, unicode):
+        return str(data)
+    elif isinstance(data, dict):
+        return dict(map(convert_from_unicode, data.iteritems()))
+    elif isinstance(data, (list, tuple, set, frozenset)):
+        return type(data)(map(convert_from_unicode, data))
+    else:
+        return data
 
 def debug(user):
     print user
@@ -52,26 +60,20 @@ def set_rule(email, **kwargs):
     user = get_user(email)
 
     #treat kwargs as the dict for another rule
-
     if kwargs.has_key('name'):
-        
         if not user.has_key('rules'):
             user['rules'] = []
         rules = user['rules']
-        
         for r in rules:
             if r['name'] == kwargs['name']:
                 return 'Rule with this name already exists'
-
         user['rules'].append(kwargs)
     
     else:
         return 'Rule must have a unique name'
 
     save_user(user)
-    
     debug(user)
-
     return 'success!'
 
 def update_rule(email, rule_name, **kwargs):
@@ -103,21 +105,34 @@ def test_time(**rule):
         if now.weekday() not in rule['valid_days']:
             return 'day of the week condition failed'
 
-def test_location(location, **rule):
+    if rule.has_key('valid_start_time'):
+        time = rule['valid_start_time']
+        if now.time.hour < int(time.split(':')[0]) and now.time.minute < int(time.split(':')[1]):
+            return 'start time condition failed'
 
-    if r['location'][0] == location[0] and r['location'][1] == location[1]:
+    if rule.has_key('valid_end_time'):
+        time = rule['valid_end_time']
+        if now.time.hour > int(time.split(':')[0]) and now.time.minute > int(time.split(':')[1]):
+            return 'end time condition failed'
+    
+    return True
+
+
+def test_location(current_location, **rule):
+
+    if rule['location'][0] == current_location[0] and rule['location'][1] == current_location[1]:
         return True  
 
     else: return False            
                 
-def check(email, location):
+def check(email, current_location):
     
     user = get_user(email)
-    if user.has_key(rules):
+    if user.has_key('rules'):
         for r in user['rules']:
-            if test_location(location, r) and test_time(r):
+            if test_location(current_location, **convert_from_unicode(r)) and test_time(**convert_from_unicode(r)):
                 #do action
-                pass
+                print 'execute action!'
 
     else:
         return "This user has no rules"
@@ -125,8 +140,10 @@ def check(email, location):
                  
 if __name__ == "__main__":
 
-    msg = delete_rule('katycorp@gmail.com', 'send an email')
+    #msg = delete_rule('katycorp@gmail.com', 'send an email')
 
-    msg = update_rule("katycorp@gmail.com", "send one email", location=[2, 2], name="send one email", action_type="email", email_address="someone@bla.net")
+    msg = set_rule("katycorp@gmail.com", name="time conditions", action_type="email", email_address="someone@bla.net", location=[2, 2], valid_days=[1,2,3,4,5], valid_start_time='12:00', valid_end_time='20:00')
     print msg
+
+    check('katycorp@gmail.com', [2, 2])
 
