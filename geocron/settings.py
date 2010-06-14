02384388dd87c139c4b5522cbc40530644e5899d
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-06-14 16:40:00 -0400

    login is working

--- geocron/settings.py
+++ geocron/settings.py
@@ -1,5 +1,8 @@
 DEBUG = True
 
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+
 try:
     from local_settings import *
 except ImportError:
--- geocron/web/__init__.py
+++ geocron/web/__init__.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, session
+from geocron import settings
+from geocron.location import Latitude
 from geocron.web.auth import auth
+from pymongo import Connection
 
 application = Flask(__name__)
 application.register_module(auth)
 
+conn = Connection(settings.MONGODB_HOST, settings.MONGODB_PORT)
+
+# request lifecycle
+
+@app.before_request
+def before_request():
+    g.db = conn.geocron
+    
+@app.after_request
+def after_request(response):
+    return response
+
+# application
+
 @application.route("/")
 def hello():
-    return "Hello World!"
+    
+    if 'access_token' in session and 'access_secret' in session:
+        l = Latitude(session['access_token'], session['access_secret'])
+        content = l.current_location()
+    else:
+        content = "asdfasdf"
+    
+    return render_template('base.html', content=content)
