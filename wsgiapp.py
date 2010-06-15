4320a60e5898adc3b2d5ad5dc938de840cf79fe3
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-06-15 15:55:25 -0400

    add file logging too

--- wsgiapp.py
+++ wsgiapp.py
@@ -4,6 +4,7 @@ from geocron import settings
 if not settings.DEBUG:
     
     import logging
+    
     from logging.handlers import SMTPHandler
     mail_handler = SMTPHandler(settings.SMTP_HOST,
                                'geocron.us@gmail.com',
@@ -12,6 +13,11 @@ if not settings.DEBUG:
                                (settings.SMTP_USER, settings.SMTP_PASSWORD))
     mail_handler.setLevel(logging.ERROR)
     application.logger.addHandler(mail_handler)
+    
+    from logging.handlers import FileHandler
+    file_handler = FileHandler("/tmp/geocron.log")
+    file_handler.setLevel(logging.WARNING)
+    application.logger.addHandler(file_handler)
 
 applications = {
     '/': application,
