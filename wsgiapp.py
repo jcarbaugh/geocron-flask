bf0cbfa5730f1e625e3478fd9a1bb28bd6d2f906
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-06-15 15:47:53 -0400

    try to do logging emails

--- wsgiapp.py
+++ wsgiapp.py
@@ -1,4 +1,17 @@
 from geocron.web import application
+from geocron import settings
+
+if not settings.DEBUG:
+    
+    import logging
+    from logging.handlers import SMTPHandler
+    mail_handler = SMTPHandler(settings.SMTP_HOST,
+                               'geocron.us@gmail.com',
+                               ADMINS,
+                               'Our application failed',
+                               (settings.SMTP_USER, settings.SMTP_PASSWORD))
+    mail_handler.setLevel(logging.ERROR)
+    application.logger.addHandler(mail_handler)
 
 applications = {
     '/': application,
