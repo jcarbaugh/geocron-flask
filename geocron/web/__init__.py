e6859b1fcc1afd06f2ff9f1f904d637803bd1e55
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-06-16 11:16:36 -0400

    add checkin page

--- geocron/web/__init__.py
+++ geocron/web/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, g, render_template, session
+from flask import Flask, g, redirect, render_template, request, session, url_for
 from geocron import settings
 from geocron.location import Latitude
+from geocron.rules import rules
 from geocron.web.auth import auth
 from pymongo import Connection
+import time
 
 application = Flask(__name__)
 application.register_module(auth)
@@ -42,7 +44,28 @@ def hello():
 @application.route("/users")
 def user_list():
     users = g.db.users.find()
-    for user in users:
-        l = Latitude(user['oauth']['token'], user['oauth']['secret'])
-        print l.locations(granularity='best')
-    return render_template('user_list.html', users=g.db.users.find())
+    return render_template('user_list.html', users=users)
+
+@application.route("/users/<identity>")
+def user_detail(identity):
+    user = g.db.users.find_one({'_id': identity})
+    return render_template('user_detail.html', user=user)
+
+@application.route("/checkin", methods=['GET', 'POST'])
+def checkin():
+    if request.method == 'POST':
+        identity = request.form['identity']
+        location = {
+            'kind': 'latitude#location',
+            'latitude': float(request.form['latitude']),
+            'longitude': float(request.form['longitude']),
+            'accuracy': int(request.form['accuracy']),
+            'timestampMs': int(time.time() * 1000),
+        }
+        user = g.db.users.find_one({'_id': identity})
+        user['locations'].insert(0, location)
+        g.db.users.save(user)
+        rules.check(identity, (location['latitude'], location['longitude']))
+        return redirect(url_for('user_detail', identity=user['_id']))
+    else:
+        return render_template('checkin.html')
